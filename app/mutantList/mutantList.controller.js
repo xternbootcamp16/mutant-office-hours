b9089f68566ac2b0b3c8ca2d15f54f0a837dd771
Seth Baughman <sbaughman1@me.com>
2016-06-20 11:37:39 -0400

    Adds Mutant constructor.  Binds form values to newMutant with ng-model

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -8,7 +8,21 @@
   function MutantListController() {
     var vm = this;
 
+    vm.addMutant = addMutant;
+    vm.newMutant = new Mutant();
     vm.mutants = ['Wolverine', 'X23', 'dave'];
+
+    function Mutant() {
+      this.name = '';
+      this.phone = '';
+      this.topic = '';
+      this.notified = false;
+      this.complete = false;
+    }
+
+    function addMutant() {
+      vm.mutants.push('professor x')
+    }
   }
 
 })();
