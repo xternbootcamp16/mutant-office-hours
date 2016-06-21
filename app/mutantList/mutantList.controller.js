ac3b7c35cf7b3f6bf6fd4d8656982be3de1db206
Seth Baughman <sbaughman1@me.com>
2016-06-21 10:21:42 -0400

    adds ng-click to text button.  Method generates a text object and saves it to firebase

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -10,12 +10,14 @@
   function MutantListController($firebaseArray) {
     var vm = this;
     var mutantsRef = firebase.database().ref().child('mutants');
+    var textsRef = firebase.database().ref().child('texts');
 
     vm.addMutant = addMutant;
     vm.newMutant = new Mutant();
     vm.mutants = $firebaseArray(mutantsRef);
     vm.toggleComplete = toggleComplete;
     vm.deleteMutant = deleteMutant;
+    vm.sendText = sendText;
 
     function Mutant() {
       this.name = '';
@@ -35,6 +37,17 @@
 
     function deleteMutant(mutant) {
       vm.mutants.$remove(mutant);
+    }
+
+    function sendText(mutant) {
+      var newText = {
+        topic: mutant.topic,
+        name: mutant.name,
+        phoneNumber: mutant.phone
+      };
+      textsRef.push(newText);
+      mutant.notified = true;
+      vm.mutants.$save(mutant);
     }
   }
 
