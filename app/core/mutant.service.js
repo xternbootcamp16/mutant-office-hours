ad3b3a2946104c9882c2eea84b0e4d9bb6cd6c65
Seth Baughman <sbaughman1@me.com>
2016-06-23 13:40:14 -0400

    Move mutant constructor to factory.  Inject mutantService into mutantList controller and refactor vm.mutant and addMutant

--- app/core/mutant.service.js
+++ app/core/mutant.service.js
@@ -6,9 +6,21 @@
     .factory('mutantService', mutantService);
 
   function mutantService() {
-    var service = {};
+    var service = {
+      Mutant: Mutant,
+    };
 
     return service;
+
+    ///////////////
+
+    function Mutant() {
+      this.name = '';
+      this.phone = '';
+      this.topic = '';
+      this.notified = false;
+      this.complete = false;
+    }
 
   }
 })();
