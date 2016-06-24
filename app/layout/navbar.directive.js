7a03d7e5c08875434b2dba6b44d603800e4a72e9
Seth Baughman <sbaughman1@me.com>
2016-06-24 11:07:04 -0400

    Add NavbarController to navbar directive

--- app/layout/navbar.directive.js
+++ app/layout/navbar.directive.js
@@ -9,6 +9,23 @@
     return {
       templateUrl: 'app/layout/navbar.html',
       restrict: 'E',
+      controller: NavbarController,
+      controllerAs: 'vm',
     };
   }
+
+  NavbarController.$inject = ['$state', 'authService'];
+
+  function NavbarController($state, authService) {
+    var vm = this;
+
+    vm.isLoggedIn = authService.isLoggedIn;
+    vm.logout = logout;
+
+    function logout() {
+      authService.logout();
+      $state.go('home');
+    }
+  }
+
 })();
