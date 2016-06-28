2f80b560ab2b3def6e667ab9ff975ba96ce40fe8
Seth Baughman <sbaughman1@me.com>
2016-06-28 14:41:45 -0400

    Remove unnecessary functions from MutantListController

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -10,17 +10,10 @@
   function MutantListController(mutantService, textMessageService, user) {
     var vm = this;
 
-    vm.addMutant = addMutant;
     vm.mutants = mutantService.mutantsByUser(user.uid);
-    vm.newMutant = new mutantService.Mutant();
     vm.deleteMutant = deleteMutant;
     vm.toggleComplete = toggleComplete;
     vm.sendText = sendText;
-
-    function addMutant() {
-      vm.mutants.$add(vm.newMutant);
-      vm.newMutant = new mutantService.Mutant();
-    }
 
     function deleteMutant(mutant) {
       vm.mutants.$remove(mutant);
