67fb7ff8bad6e090f4b48b63b5ab1aa50749ab05
Seth Baughman <sbaughman1@me.com>
2016-06-28 15:33:41 -0400

    Add controller for mutantTable.  Copy needed functions and variables from mutantList controller.  Create isolate scope and bind mutants property.  Remove unneeded functions from mutantList controller

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -5,27 +5,12 @@
     .module('mutantApp.mutantList')
     .controller('MutantListController', MutantListController);
 
-  MutantListController.$inject=['mutantService', 'textMessageService', 'user'];
+  MutantListController.$inject=['mutantService', 'user'];
 
-  function MutantListController(mutantService, textMessageService, user) {
+  function MutantListController(mutantService, user) {
     var vm = this;
 
     vm.mutants = mutantService.mutantsByUser(user.uid);
-    vm.deleteMutant = deleteMutant;
-    vm.toggleComplete = toggleComplete;
-    vm.sendText = sendText;
-
-    function deleteMutant(mutant) {
-      vm.mutants.$remove(mutant);
-    }
-
-    function toggleComplete(mutant) {
-      vm.mutants.$save(mutant);
-    }
-
-    function sendText(mutant) {
-      textMessageService.sendText(mutant, vm.mutants);
-    }
 
   }
 })();
