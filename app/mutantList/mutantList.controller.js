a059dfd8cb5475e0ef8c46b61f6e01e5a259fd2f
Seth Baughman <sbaughman1@me.com>
2016-06-28 10:36:02 -0400

    Add MutantFormController with vm.newMutant and vm.addMutant. Add isolate scope to directive and pass in vm.mutants with two-way binding

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -10,17 +10,10 @@
   function MutantListController(mutantService, firebaseDataService, textMessageService, user) {
     var vm = this;
 
-    vm.addMutant = addMutant;
-    vm.newMutant = new mutantService.Mutant();
     vm.mutants = mutantService.mutantsByUser(user.uid);
     vm.toggleComplete = toggleComplete;
     vm.deleteMutant = deleteMutant;
     vm.sendText = sendText;
-
-    function addMutant() {
-      vm.mutants.$add(vm.newMutant);
-      vm.newMutant = new mutantService.Mutant();
-    }
 
     function toggleComplete(mutant) {
       vm.mutants.$save(mutant);
