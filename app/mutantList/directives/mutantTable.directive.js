62f1a4e8e812657114aa611bbb2fb271f0a68a07
Seth Baughman <sbaughman1@me.com>
2016-06-28 11:31:01 -0400

    Add markup to mutantTable.html.  Add controller to mutantTable directive and transfer functions from mutantList controller.  Add isolate scope to the directive and pass vm.mutants to it

--- app/mutantList/directives/mutantTable.directive.js
+++ app/mutantList/directives/mutantTable.directive.js
@@ -9,6 +9,34 @@
     return {
       templateUrl: 'app/mutantList/directives/mutantTable.html',
       restrict: 'E',
+      controller: MutantTableController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        mutants: '=',
+      }
     };
+  }
+
+  MutantTableController.$inject = ['textMessageService'];
+
+  function MutantTableController(textMessageService) {
+    var vm = this;
+
+    vm.deleteMutant = deleteMutant;
+    vm.sendText = sendText;
+    vm.toggleComplete = toggleComplete;
+
+    function toggleComplete(mutant) {
+      vm.mutants.$save(mutant);
+    }
+
+    function deleteMutant(mutant) {
+      vm.mutants.$remove(mutant);
+    }
+
+    function sendText(mutant) {
+      textMessageService.sendText(mutant, vm.mutants);
+    }
   }
 })();
--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -5,27 +5,13 @@
     .module('mutantApp.mutantList')
     .controller('MutantListController', MutantListController);
 
-  MutantListController.$inject = ['mutantService', 'firebaseDataService', 'textMessageService', 'user'];
+  MutantListController.$inject = ['mutantService', 'user'];
 
-  function MutantListController(mutantService, firebaseDataService, textMessageService, user) {
+  function MutantListController(mutantService, user) {
     var vm = this;
 
     vm.mutants = mutantService.mutantsByUser(user.uid);
-    vm.toggleComplete = toggleComplete;
-    vm.deleteMutant = deleteMutant;
-    vm.sendText = sendText;
 
-    function toggleComplete(mutant) {
-      vm.mutants.$save(mutant);
-    }
-
-    function deleteMutant(mutant) {
-      vm.mutants.$remove(mutant);
-    }
-
-    function sendText(mutant) {
-      textMessageService.sendText(mutant, vm.mutants);
-    }
   }
 
 })();
