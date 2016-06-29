c6c6e64f53e340630f9db53636d7b69c54c33a3f
Seth Baughman <sbaughman1@me.com>
2016-06-29 15:05:57 -0400

    Set up authForm directive and add script tag on index.html

---
+++ app/auth/directives/authForm.directive.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mutantApp.auth')
+    .directive('xtAuthForm', xtAuthForm);
+
+  function xtAuthForm() {
+    return {
+      templateUrl: 'app/auth/directives/authForm.html',
+      restrict: 'E',
+      controller: AuthFormController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+
+      }
+    };
+  }
+
+  function AuthFormController() {
+    var vm = this;
+  }
+
+})();
